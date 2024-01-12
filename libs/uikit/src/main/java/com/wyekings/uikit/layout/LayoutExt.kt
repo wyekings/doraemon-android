278bb9bad4bc4b084957e395d2d037c9d16675cc
wyekings <wyekings@outlook.com>
2024-01-12 11:51:17 +0800

    Add custom layout

---
+++ libs/uikit/src/main/java/com/wyekings/uikit/layout/LayoutExt.kt
@@ -0,0 +1,6 @@
+package com.wyekings.uikit.layout
+
+import android.view.ViewGroup
+
+const val MATCH_PARENT = ViewGroup.LayoutParams.MATCH_PARENT
+const val WRAP_CONTENT = ViewGroup.LayoutParams.WRAP_CONTENT
